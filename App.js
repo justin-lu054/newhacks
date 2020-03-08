77e43d3332ae64e1c563f380c7a1783199245441
Andrew Ma <ayma01@gmail.com>
2020-03-07 23:11:57 -0500

    Adding stack navigation for the application

--- App.js
+++ App.js
@@ -1,75 +1,66 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MainMenu from './screens/mainmenu'
-import GetHome from './screens/home';
-import Food from './screens/food';
-import GetLocation from 'react-native-get-location'
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MainMenu from "./screens/mainmenu";
+import GetHome from "./screens/home";
+import Food from "./screens/food";
+import GetLocation from "react-native-get-location";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-
-function getLocation () {
-  //return new Promise((resolve,reject) => {
-      GetLocation.getCurrentPosition({
-          enableHighAccuracy: true,
-          timeout: 15000,
-      })
-      .then(location => {
-          console.log(location);
-      })
-      .catch(error => {
-          console.log(error);
-      })
-  //})
-}
-
-export default class App extends Component {
-
-  state = {
-    screen: "main"
-  }
-
-  showFood = () => {
-    this.setState({screen: "food"});
-  }
-
-  showHome = () => {
-    this.setState({screen: "home"}); 
-  }
-
-  getComponent = () => {
-    var component; 
-    if (this.state.screen === "main") {
-      component = <MainMenu showFood={this.showFood} showHome={this.showHome}></MainMenu>;
-    }
-    else if (this.state.screen === "food") {
-      component = <Food></Food>;
-    }
-    else if (this.state.screen === "home") {
-      component = <GetHome></GetHome>
-    }
-    return component; 
-  }
-
-  onComponentMount(){ 
-    getLocation()
-  }
-
-  render() {
-    
-    return (
-      <React.Fragment>
-        
-        {this.getComponent()}
-      </React.Fragment>
-      
-    );
-  }
-}
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+function getLocation() {
+  //return new Promise((resolve,reject) => {
+  GetLocation.getCurrentPosition({
+    esnableHighAccuracy: true,
+    timeout: 15000
+  })
+    .then(location => {
+      console.log(location);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  //})
+}
+
+//Stack Navigator for App Button Navigation
+export default class App extends Component {
+  /*
+  state = {
+    isLoadingComplete: false
+  };
+
+  handleFinishLoading() {
+    this.setState({ isLoadingComplete: true });
+  }
+  */
+  onComponentMount() {
+    getLocation();
+  }
+
+  myStack() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="MainMenu">
+          <Stack.Screen name="MainMenu" component={MainMenu} />
+          <Stack.Screen name="GetHome" component={GetHome} />
+          <Stack.Screen name="Food" component={Food} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+  render() {
+    return this.myStack();
+  }
+}
--- screens/mainmenu.jsx
+++ screens/mainmenu.jsx
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, Button } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center"
-    },
-    buttonContainer: {
-        margin: 20
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    margin: 20
+  }
 });
 
-
 class MainMenu extends Component {
-    state = {
-        
-    };
+  state = {};
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.buttonContainer}>
-                    <Button title="Food" onPress={this.props.showFood} />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button title="Home" onPress={this.props.showHome} />
-                </View>
-            </View>
-        );
-    }
+  render() {
+    const { navigation } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button title="Food" onPress={() => navigation.navigate("Food")} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Home" onPress={() => navigation.navigate("GetHome")} />
+        </View>
+      </View>
+    );
+  }
 }
-
 
 export default MainMenu;
