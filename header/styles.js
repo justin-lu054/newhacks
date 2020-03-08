e5c9667252bd2a4783240f28517f975c033c6fda
Andrew Ma <ayma01@gmail.com>
2020-03-07 23:39:33 -0500

    resolve conflicts

--- header/styles.js
+++ header/styles.js
@@ -1,16 +1,13 @@
 import { StyleSheet } from "react";
 
-<<<<<<< HEAD
 const styles = StyleSheet.create({});
-=======
 const styles = StyleSheet.create({
-    container: {
-        position: absolute,
-        left: 0,
-        right:0,
-        top: 0
-    }
-})
->>>>>>> a2a5734b6eeb4bff8fe5531a8cb586e48fd585a7
+  container: {
+    position: absolute,
+    left: 0,
+    right: 0,
+    top: 0
+  }
+});
 
 export default styles;
